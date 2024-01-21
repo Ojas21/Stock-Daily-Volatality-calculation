528d6d177dab91177878432d9272dce589bb7f05
Ojas21 <65960254+Ojas21@users.noreply.github.com>
2024-01-21 19:50:20 +0530

    minor Update Finzome-Assign_Task_1.py

--- Finzome-Assign_Task_1.py
+++ Finzome-Assign_Task_1.py
@@ -1,30 +1,40 @@
 import pandas as pd
 import numpy as np
 
-# Read data from Excel file
+# file path
 file_path = 'C:\\path\\test.csv'
 
+# reading data from the csv file
 df = pd.read_csv(file_path)
 
-# Adjust column names by removing extra spaces and special characters
+# Adjusting the column names by removing extra spaces and special characters, to extract data without any issues
 df.columns = df.columns.str.strip().str.replace(' ', '_', regex=True).str.replace('₹', '', regex=True).str.replace('\(','', regex=True).str.replace('\)','', regex=True)
 
-# Check column names
+# Checking the column names
 print(df.columns)
 
-# Adjust date column name if needed
+# Used to Adjust the date column name(optional)
 df['Date'] = pd.to_datetime(df['Date'], format='%d-%b-%y', dayfirst=True)
 
-# Handle missing values if needed
-# df = df.fillna(0)  # Replace NaN with 0 or use other appropriate methods
+# Calculating Daily Returns
+'''
+for testing purposes
+current_close = 18107.85
+previous_close = 18165.35
 
-# Calculate Daily Returns
-df['Daily_Returns'] = df['Close'].pct_change()
+daily_returns = (current_close / previous_close) - 1
 
-# Calculate Daily Volatility
+print("Daily Returns:", daily_returns)
+answer= -0.003165
+'''
+df['Daily_Returns'] = df['Close'] / df['Close'].shift(1) - 1
+
+#print(df['Daily_Returns'])
+
+# Calculating Daily Volatility
 daily_volatility = df['Daily_Returns'].std()
 
-# Calculate Annualized Volatility
+# Calculating Annualized Volatility
 length_of_data = len(df)
 annualized_volatility = daily_volatility * np.sqrt(length_of_data)
 
